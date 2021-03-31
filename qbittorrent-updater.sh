830098962acf004c9e7759273db63d44f4d61b83
Madagambada <47931256+Madagambada@users.noreply.github.com>
2021-03-31 12:24:37 +0200

    Update and rename update.sh to qbittorrent-updater.sh

--- qbittorrent-updater.sh
+++ qbittorrent-updater.sh
@@ -5,12 +5,18 @@ local_version=$(qbittorrent-nox -v | sed 's/.*v//')
 
 if [ $remote_version != $local_version ]; then
   wget -q https://github.com/Madagambada/qbittorrent-nox-builder/releases/download/$remote_version/qbittorrent-nox_$remote_version-1_i386.deb
-  
+
+  dpkg -x qbittorrent-nox_$remote_version-1_i386.deb new
+  dpkg -e qbittorrent-nox_$remote_version-1_i386.deb new/DEBIAN
+  #Change madagambada so that it fits your username
+  sed -i 's/#User=/User=madagambada/g' new/etc/systemd/system/qbittorrent-nox.service
+  dpkg-deb --build new
+
   systemctl stop qbittorrent-nox
-  
-  apt install qbittorrent-nox_$remote_version-1_i386.deb -y
-  rm qbittorrent-nox_$remote_version-1_i386.deb
-  
+
+  apt install new.deb -y
+  rm qbittorrent-nox_$remote_version-1_i386.deb new new.deb
+
   systemctl --system daemon-reload
   systemctl enable qbittorrent-nox
   systemctl start qbittorrent-nox
