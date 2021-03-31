6218a4d658ba9da3b39cc2f57d0f884cb4f5143d
Madagambada <47931256+Madagambada@users.noreply.github.com>
2021-03-31 12:29:31 +0200

    Create cloudflared-updater.sh

---
+++ cloudflared-updater.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+remote_version=$(curl  https://api.github.com/repos/cloudflare/cloudflared/tags -s | grep name | sed 's/    "name":\ "//' | sed 's/",//' | sort -V | tail -1)
+local_version=$(cloudflared -v | sed 's/.*n //' | sed 's/ (.*//')
+
+if [ $remote_version != $local_version ]; then
+  cloudflared update
+  systemctl restart cloudflared
+fi
