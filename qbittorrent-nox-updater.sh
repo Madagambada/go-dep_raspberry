5f54730f65cb79f1de987b1c933a6d90eb3c3d58
Madagambada <47931256+Madagambada@users.noreply.github.com>
2021-04-02 01:52:42 +0200

    Update and rename qbittorrent-updater.sh to qbittorrent-nox-updater.sh

--- qbittorrent-nox-updater.sh
+++ qbittorrent-nox-updater.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 remote_version=$(curl  https://api.github.com/repos/Madagambada/qbittorrent-nox-static-i386/tags -s | grep name | sed 's/    "name":\ "//' | sed 's/",//' | sort -V | tail -1)
-local_version=$(qbittorrent-nox -v | sed 's/.*v//')
+if [ -z $remote_version ]
+then
+  remote_version="0"
+fi
+
+if [ -f "/usr/local/bin/qbittorrent-nox" ]
+then
+  local_version=$(qbittorrent-nox -v | sed 's/.*v//')
+else
+  local_version="0"
+fi
 
 if [ $remote_version != $local_version ]; then
   wget -q https://github.com/Madagambada/qbittorrent-nox-static-i386/releases/download/$remote_version/qbittorrent-nox_$remote_version-1_i386.deb
