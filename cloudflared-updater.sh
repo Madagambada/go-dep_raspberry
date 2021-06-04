479d6198a86246db3ebbcb10c4804009ace88e5b
Madagambada <47931256+Madagambada@users.noreply.github.com>
2021-06-04 10:05:42 +0200

    Update cloudflared-updater.sh

--- cloudflared-updater.sh
+++ cloudflared-updater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 remote_version=$(curl  https://api.github.com/repos/cloudflare/cloudflared/tags -s | grep name | sed 's/    "name":\ "//' | sed 's/",//' | sort -V | tail -1)
-local_version=$(cloudflared -v | sed 's/.*n //' | sed 's/ (.*//')
+local_version=$(/usr/local/bin/cloudflared -v | sed 's/.*n //' | sed 's/ (.*//')
 
 if [ $remote_version != $local_version ]; then
   /usr/local/bin/cloudflared update
